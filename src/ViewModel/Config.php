5d6ca2b7a2d9f2d9a6d91e8dc5c6e5ac112213da
Romain [ORION] Genin <romain.genin@dnd.fr>
2025-06-05 10:59:10 +0200

    HIPAMAG2004-33: Paypal V2 - TOC

--- src/ViewModel/Config.php
+++ src/ViewModel/Config.php
@@ -95,4 +95,9 @@ class Config implements ArgumentInterface
     {
         return (bool) $this->payPalConfigProvider->getConfig()['payment']['hipay_paypalapi']['isPayPalV2'] ?? false;
     }
+
+    public function isTOCEnabled(): bool
+    {
+        return (bool) $this->scopeConfig->isSetFlag('checkout/options/enable_agreements', ScopeInterface::SCOPE_STORE) ?? false;
+    }
 }
